5deb8a5d7d73f3b22bed9ce9c8943a4bea292022
Sarmed <sarmed.shaya@gmail.com>
2022-09-16 23:37:50 -0400

    Initialized pygame controller and thinking of using the DPAD and main buttons mostly for all fucntions

--- NAMETBD.py
+++ NAMETBD.py
@@ -1,3 +1,22 @@
 import socket
 import pygame as pg
 
+loop = True
+
+pg.init()
+pg.joystick.init()
+joysticks = [pg.joystick.Joystick(x) for x in range(pg.joystick.get_count())]
+
+#print(pg.joystick.Joystick.get_id())
+
+while loop:
+    for event in pg.event.get():
+        if event.type == pg.JOYBUTTONDOWN:
+            print(event)
+            exit(0)
+        elif event.type == pg.JOYHATMOTION:
+            print(event)
+
+
+
+
