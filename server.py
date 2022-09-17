489d18691f1606128e80a064b09bc290a30e09f9
Sarmed <sarmed.shaya@gmail.com>
2022-09-17 14:23:35 -0400

    fully implemented the server side of things only changes will come at the end

--- server.py
+++ server.py
@@ -1,24 +1,35 @@
+from http import server
 import socket
+from sqlite3 import connect
+import threading
 
-host = ''
-port = 5555
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host,port))
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DC_MSG = "!DISCONNECT"
 
-print (host+' '+port)
-s.listen(1)
-conn, addr = s.accept()
-print('Connected by', addr)
-while True:
-    try:
-        data = conn.recv(1024)
-        if not data: break
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #using TCP because why not
+server.bind(ADDR)
 
-        print ("Client Says: "+data)
-        conn.sendall("Server Says:hi")
+def handle_client(conn,addr):
+    print("New Client: " + str(addr))
+    connected = True
+    while connected:
+        msg = conn.recv(1024).decode(FORMAT) #1kb is way more than neccessary, can optimize but shouldn't cause problems
+        if msg == DC_MSG:
+            connected = False
+        print(msg)
+    conn.close()
 
-    except socket.error:
-        print ("Error Occured.")
-        break
+def start():
+    server.listen()
+    print(f"Server is listening: {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print("Active Connections = " + str(threading.active_count()-1)) #sub 1 because start thread counts as 1 thread
 
-conn.close()
+print("Server initialized...")
+start()
