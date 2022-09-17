9ed670e9b52bcd38ebf055040c1878894b5b9a8d
Sarmed <sarmed.shaya@gmail.com>
2022-09-16 23:59:29 -0400

    server side is where i will put send packets and print

---
+++ controller.py
@@ -0,0 +1,27 @@
+import socket
+import pygame as pg
+
+loop = True
+
+
+pg.init()
+pg.joystick.init()
+joysticks = [pg.joystick.Joystick(x) for x in range(pg.joystick.get_count())]
+
+#print(pg.joystick.Joystick.get_id())
+
+while loop:
+    for event in pg.event.get():
+        if event.type == pg.JOYBUTTONDOWN:
+            print(event)
+            
+            exit(0) #for testing
+        elif event.type == pg.JOYHATMOTION:
+            print(event)
+        elif event.type == pg.JOYDEVICEREMOVED:
+            print('EXITTING DEVICE REMOVED')
+            exit(0)
+
+
+
+
