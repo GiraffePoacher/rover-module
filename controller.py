2511e2257d6dfc4c80cd010de95d713c61709c6c
Sarmed <sarmed.shaya@gmail.com>
2022-09-17 17:24:16 -0400

    client side test

--- controller.py
+++ controller.py
@@ -1,29 +1,26 @@
 import socket
 import pygame as pg
 
-host = socket.gethostname()
-port = 5555
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+FORMAT = 'utf-8'
+DC_MSG = "!DISCONNECT"
+ADDR = (SERVER, PORT)
 
-loop = True
-
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
 
 pg.init()
 pg.joystick.init()
 joysticks = [pg.joystick.Joystick(x) for x in range(pg.joystick.get_count())]
 
-#print(pg.joystick.Joystick.get_id())
+def send(msg):
+    message = msg.encode(FORMAT)
+    client.send(message)
 
-while loop:
-    for event in pg.event.get():
-        if event.type == pg.JOYBUTTONDOWN:
-            print(event)
-            
-            exit(0) #for testing
-        elif event.type == pg.JOYHATMOTION:
-            print(event)
-        elif event.type == pg.JOYDEVICEREMOVED:
-            print('EXITTING DEVICE REMOVED')
-            exit(0)
+
+send("hu")
+
 
 
 
