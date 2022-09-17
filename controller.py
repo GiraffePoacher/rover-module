4fb970593bf6e6a385ab23ce8a831da5d2ed7c2e
Sarmed <sarmed.shaya@gmail.com>
2022-09-17 18:12:28 -0400

    re-added the pygame code

--- controller.py
+++ controller.py
@@ -10,17 +10,28 @@ ADDR = (SERVER, PORT)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-pg.init()
-pg.joystick.init()
-joysticks = [pg.joystick.Joystick(x) for x in range(pg.joystick.get_count())]
-
 def send(msg):
     message = msg.encode(FORMAT)
     client.send(message)
 
+pg.init()
+pg.joystick.init()
+joysticks = [pg.joystick.Joystick(x) for x in range(pg.joystick.get_count())]
+#think about appropriate data set for the wheel stuff
 
-send("hu")
-send(DC_MSG)
+while True:
+    for event in pg.event.get():
+        if event.type == pg.JOYBUTTONDOWN:
+            print(event)
+            
+            exit(0) #for testing
+        elif event.type == pg.JOYHATMOTION:
+            print(event)
+        elif event.type == pg.JOYDEVICEREMOVED:
+            print('EXITTING DEVICE REMOVED')
+            send(DC_MSG)
+            exit(0)
+
 
 
 
